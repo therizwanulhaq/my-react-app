e32439b80225320d6ea76778f64bc8363741169c
rizu <therizwanulhaq@gmail.com>
2023-09-26 23:16:31 +0530

    added routes

---
+++ src/components/Pages.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home() {
+  return <div>Home Page</div>;
+}
+
+function About() {
+  return <div>About Page</div>;
+}
+
+function Contact() {
+  return <div>Contact Page</div>;
+}
+
+export { Home, About, Contact };
